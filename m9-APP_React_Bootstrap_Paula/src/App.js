8d12799e509521907682c59019d47718e58ed034
MONLAU\paulaalafil <Paula del Alamo>
2023-03-29 19:24:21 +0200

    APP - React Bootstrap / Tailwind - 01

---
+++ m9-APP_React_Bootstrap_Paula/src/App.js
@@ -0,0 +1,15 @@
+import './App.css';
+import { Button } from 'react-bootstrap';
+
+function App() {
+  return (
+
+<div>
+<h1>ONG Title</h1>
+<p>Información sobre la ONG...</p>
+<Button variant="primary">Donar</Button>
+</div>
+);
+}
+
+export default App;
